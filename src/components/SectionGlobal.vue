e30e352eab3d25727e4cbeea477cf14d91b17ed4
KumaBar <albermangutov@yandex.ru>
2021-10-11 21:30:32 +0300

    создал SectionGlobal

---
+++ src/components/SectionGlobal.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class='global'>
+        <div class='global-title'>Huge Global Network <br/> of Fast VPN</div>
+        <div class='global-text'>See LaslesVPN everywhere to make it easier for you when you move locations.</div>
+        <div class='global-img'>
+            <img src="../assets/Huge Global.png" alt="">
+        </div>
+    </div>
+</template>
+
+<style>
+.global{
+    background:white;
+}
+.global-title{
+    font-size:35px;
+    font-weight:500;
+    color:black;
+    line-height:50px;
+    margin-bottom:20px;
+    text-align:center;
+    padding-top:20px;
+}
+.global-text{
+    line-height:30px;
+    margin-bottom:20px;
+    text-align:center;
+    margin-bottom:150px;
+}
+.global-img img{
+display:block;
+width: 100%;
+max-width:1060px;
+margin-bottom:40px;
+}
+@media(max-width:600px){
+    .global-title{
+        font-size:30px;
+    }
+}
+</style>
